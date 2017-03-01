34ccace32e26e1c0b4f6f09f30034c0331a28d7a
redpath <redpath@nscl.msu.edu>
2017-03-01 17:40:35 -0500

    added Binning.h

---
+++ simulation/sens/PreGeant/Binning.h
@@ -0,0 +1,58 @@
+/* Set binning parameters for histograms made in PreGeant.C */
+   /* "best case" 
+   Int_t nbinsx = 100; Double_t x1=-5; Double_t x2=5;
+   Int_t nbinsy = 100; Double_t y1=-5; Double_t y2=5;
+   Int_t nbinske= 100; Double_t ke1=61.,ke2=62.;
+   Int_t nbinsv = 100; Double_t v1 =10.36,v2=10.46;
+   Int_t nbinsvr= 100; Double_t vr1=-1.,vr2=1.;
+   Int_t nbinst = 100; Double_t t1 = 20;Double_t t2 = 40;
+   */
+   /* "best case" + 1 MeV */
+   Int_t nbinsx = 100; Double_t x1=-50; Double_t x2=50;
+   Int_t nbinsy = 100; Double_t y1=-50; Double_t y2=50;
+   Int_t nbinske= 100; Double_t ke1=45.,ke2=85.;
+   Int_t nbinsv = 60; Double_t v1 =9.0,v2=12.0;
+   Int_t nbinsvr= 150; Double_t vr1=-1.5,vr2=1.5;
+   Int_t nbinst = 60; Double_t t1 = 28;Double_t t2 = 34;
+   
+
+   /* "best case" + MoNA resolutions 
+   Int_t nbinsx = 100; Double_t x1=-5; Double_t x2=5;
+   Int_t nbinsy = 100; Double_t y1=-5; Double_t y2=5;
+   Int_t nbinske= 100; Double_t ke1=54.,ke2=64.;
+   Int_t nbinsv = 60; Double_t v1 =10.3,v2=10.5;
+   Int_t nbinsvr= 100; Double_t vr1=-1.,vr2=1.;
+   Int_t nbinst = 100; Double_t t1 = 20;Double_t t2 = 40;
+   */
+   /* 1MeV txty 
+   Int_t nbinsx = 150; Double_t x1=-150; Double_t x2=150;
+   Int_t nbinsy = 150; Double_t y1=-150; Double_t y2=150;
+   Int_t nbinske= 80; Double_t ke1=40.,ke2=80.;
+   Int_t nbinsv = 80; Double_t v1 =9.0,v2=12.0;
+   Int_t nbinsvr= 150; Double_t vr1=-1.5,vr2=1.5;
+*/
+   /* Beam tx,ty spread (w/ ToF resolution 
+   Int_t nbinsx = 150; Double_t x1=-150; Double_t x2=150;
+   Int_t nbinsy = 150; Double_t y1=-150; Double_t y2=150;
+   Int_t nbinske= 80; Double_t ke1=52.,ke2=72.;
+   Int_t nbinsv = 80; Double_t v1 =9.5,v2=11.5;
+   Int_t nbinsvr= 150; Double_t vr1=-0.75,vr2=0.75;
+   Int_t nbinst = 100; Double_t t1 = 20;Double_t t2 = 40;
+*/
+   /* Beam x,y spread 
+   Int_t nbinsx = 150; Double_t x1=-150; Double_t x2=150;
+   Int_t nbinsy = 150; Double_t y1=-150; Double_t y2=150;
+   Int_t nbinske= 80; Double_t ke1=53.,ke2=73.;
+   Int_t nbinsv = 80; Double_t v1 =9.5,v2=11.5;
+   Int_t nbinsvr= 100; Double_t vr1=-0.5,vr2=0.5;
+   Int_t nbinst = 100; Double_t t1 = 20;Double_t t2 = 40;
+*/
+   /* Real Target 
+   Int_t nbinsx = 150; Double_t x1=-150; Double_t x2=150;
+   Int_t nbinsy = 150; Double_t y1=-150; Double_t y2=150;
+   Int_t nbinske= 80; Double_t ke1=55.,ke2=95.;
+   Int_t nbinsv = 80; Double_t v1 =9.5,v2=12.5;
+   Int_t nbinsvr= 100; Double_t vr1=-1.0,vr2=1.0;
+   Int_t nbinst = 100; Double_t t1 = 20;Double_t t2 = 40;
+*/
+
